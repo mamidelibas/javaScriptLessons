4e6b649c7c106780e43ea68093f2e89cea9d5486
mamidelibas <mamidlbs@gmail.com>
2023-08-06 15:33:51 +0300

    Js

--- JAVASCRIPT/2-app.js
+++ JAVASCRIPT/2-app.js
@@ -1,3 +1,18 @@
 // ! -----------------------------------------------------------------
-console.log("----------------  ---------------");
+console.log("---------------- ARRAY ---------------");
 // ! -----------------------------------------------------------------
+
+let ogrenciler = ["Canberk", "Ahmet", "Mehmet", "Veli", 5, "Fatma", "Mami"];
+console.log("Öğrenciler Array'i : " + ogrenciler);
+
+let ilkOgrenci = ogrenciler[0];
+console.log("İlk öğrenci : " + ilkOgrenci);
+
+let toplamOgrenciSayisi = ogrenciler.length;
+console.log("Dizinin uzunluğu, öğrenci sayısı : " + toplamOgrenciSayisi);
+
+let sonOgrenci = ogrenciler[toplamOgrenciSayisi - 1];
+console.log("Son Eleman : " + sonOgrenci);
+
+ogrenciler.push("Sevgi"); // push : Dizinin sonuna eleman eklemek için kullanılır
+console.log(ogrenciler);
