126fd80a65b6fd92795c42ea9811c82518015f49
mamidelibas <mamidlbs@gmail.com>
2023-08-06 15:45:32 +0300

    Js

--- JAVASCRIPT/2-app.js
+++ JAVASCRIPT/2-app.js
@@ -16,3 +16,29 @@ console.log("Son Eleman : " + sonOgrenci);
 
 ogrenciler.push("Sevgi"); // push : Dizinin sonuna eleman eklemek için kullanılır
 console.log(ogrenciler);
+
+//! PRATİK ÖRNEK
+
+let meyve = [
+  "Çilek",
+  "Erik",
+  "Karpuz",
+  "Şeftali",
+  "kayısı",
+  "Ananas",
+  "Armut",
+  "Elma",
+  "Nektari",
+  "Ayva",
+];
+console.log(meyve);
+
+meyve.push("Kiraz");
+console.log(meyve);
+
+let ilkMeyve = meyve[0];
+console.log("İlk meyve : " + ilkMeyve);
+
+let toplamMeyve = meyve.length;
+let sonMeyve = meyve[toplamMeyve - 1];
+console.log("Son meyve : " + sonMeyve);
