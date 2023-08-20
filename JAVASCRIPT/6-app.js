61759bc39c8f7a367c242cfb7ae120e8aba5da96
mamidelibas <mamidlbs@gmail.com>
2023-08-20 16:01:45 +0300

    js7

--- JAVASCRIPT/6-app.js
+++ JAVASCRIPT/6-app.js
@@ -13,7 +13,8 @@ function konsolaYaz() {
 }
 konsolaYaz();
 
-//! PARAMETRE ATAMA
+//! ----------- 
+console.log("---------------- PARAMETRE ATAMA");
 
 function sayaciGüncelle(yeniDeger) {
   sayac = yeniDeger;
@@ -21,7 +22,79 @@ function sayaciGüncelle(yeniDeger) {
 }
 sayaciGüncelle(15);
 
-//! ÖRNEK ÖDEV
+let konsolYazisi="Selam!";
+let konsolYazisi2="Merhaba!";
+let konsolSayi;
+
+function konsolaYazdir2(text1,text2,numb=20){
+  console.log(text1,text2,numb)
+}
+konsolaYazdir2(konsolYazisi,konsolYazisi2,konsolSayi);
+
+//! ----------- 
+console.log("---------------- ARROW FUNCTİON");
+
+
+const arrowFunc=(param1,param2)=>{
+  console.log("ArrowFunc : ",param1+"  "+param2)
+}
+arrowFunc("İlk parametre","İkinci parametre")
+
+//! ----------- 
+console.log("---------------- SKOP VE RETURN : Değer döndürme");
+
+function sayilariCarp(num1,num2){
+  const carpimSonucu=num1*num2
+  console.log("Çarpma sonucu :",carpimSonucu)
+}
+sayilariCarp(10,5)
+// console.log(carpimSonucu); // FONKSYON İÇİNDE TANIMLANAN DEĞERLER SADECE PARANTEZ İÇİNDE ERİŞİLİR DIŞARDAN ERİŞİLEMEZ
+
+function sayilariBol(num1,num2){
+  const bolmeSonucu=num1/num2
+  console.log("Bölme sonucu :",bolmeSonucu)
+}
+sayilariBol(100,10)
+
+function sayilariCikart(num1,num2){
+  return(num1-num2)
+}
+const cikartmaSonucu=sayilariCikart(50,20);
+console.log("Çıkartma - RETURN - sonucu :",cikartmaSonucu)
+
+//! ---------- 
+console.log("---------------- İNTERVAL - SET TİME OUT");
+
+let sayacc=0;
+function ekranaBas(){
+  sayacc++;
+  // console.log("SetTime Sayacın güncel değeri!",sayacc)
+}
+// setTimeout(ekranaBas,2000) // Sayfa ilk yüklendikten 2 saniye sonra ekrana fonk çağır
+
+// setInterval(ekranaBas,3000) // her 3 saniyede bir ekrana bas fonksiyonu çalıştırılır
+// setInterval(()=>{
+// sayacc++;
+// console.log("INTER Syacın güncel değeri : ",sayacc)
+// },3000)
+
+//! arrow function özellik
+
+// const arrowFun=(sayi1,sayi2)=>{
+//   return sayi1*sayi2
+// }
+// const carpimSonucu2=arrowFun(5,3);
+// console.log(carpimSonucu2)
+
+// YERİNE
+
+const arrowFun=(sayi1,sayi2)=>sayi1*sayi2
+
+const carpimSonucu2=arrowFun(5,3);
+console.log(carpimSonucu2)
+
+//! ÖRNEK
+console.log("---------------- FUNCTİON İÇİNDE WHİLE ÖRNEK");
 
 let newNumber = [];
 let x = 0;
@@ -31,6 +104,20 @@ function number() {
     newNumber.push(x);
     x++;
   }
-  console.log(newNumber);
+  // console.log(newNumber);
 }
 number();
+
+//! ÖRNEK
+
+const array=[];
+let i=0;
+
+let odev=()=>{
+while(array.length<10){
+  i++
+  array.push(i)
+  console.log(array)
+}
+}
+setInterval(odev,1000)
---
+++ JAVASCRIPT/7-app.js
@@ -0,0 +1,23 @@
+console.log("---------------- DOM MANİPULATİON ");
+// JavaScript ile Html dökümanına yeni elementler ekleme silme değiştirmek icin kullanılır.
+
+//! ELEMANLARI YAKALAMAK
+let secondText = document.querySelector("#secondText");
+// let secondText = document.getElementById("secondText");
+console.log(secondText); // id normal şekilde döner id'si aynı olan İLK olanı getirir
+let thirdText = document.getElementsByClassName("thirdText");
+console.log(thirdText); // class dizi olarak döner kaç eleman varsa hepsini
+
+function yaziyiGuncelle() {
+  document.getElementById("firstText").innerHTML = "Selam Mami";
+  thirdText[1].innerHTML = "değişti";
+}
+setTimeout(yaziyiGuncelle, 2000);
+
+let degistir = document.getElementsByClassName("h1Tag");
+
+function odev() {
+  document.getElementById("pTag").innerHTML = "Muhammet Delibaş";
+  degistir[1].innerHTML = "20-03-2023";
+}
+setTimeout(odev, 2000);
