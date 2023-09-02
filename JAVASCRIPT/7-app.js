3a614069b3595ab8ad7d043aaa6dacaa47380330
mamidelibas <mamidlbs@gmail.com>
2023-09-02 15:58:47 +0300

    js

--- JAVASCRIPT/7-app.js
+++ JAVASCRIPT/7-app.js
@@ -65,16 +65,124 @@ console.log("---------------- ELEMANLAR ARASI GEÇİŞLER ");
 // const gorevler = gorevListesi.children; // tüm elamanlar
 // // console.log(gorevler);
 
-console.log("---------------- ATTİRBUTE : getElement setElement ");
+console.log(
+   "---------------- ATTİRBUTE : getElement setElement removeElement "
+);
 
-let googleLink = document.querySelector(".besinciKisim");
-console.log(googleLink);
+// let firstTag = document.querySelector("#aTag");
+// console.log(firstTag);
 
-googleLink.setAttribute("target", "_blank");
+// //! SETATTRİBUTE
 
-let di = document.querySelector(".di");
-function divSekillendir() {
-  di.setAttribute("width", "200px", "height", "200px");
+// firstTag.setAttribute("href", "https://google.com");
+// firstTag.style.color = "green";
+
+// console.log("Linkin rengi :", firstTag.style.color);
+
+// //! Classları elde etmek icin
+
+// console.log("FirstTag Elemanının Class'ı:", firstTag.className);
+
+// firstTag.className += " yeni";
+
+// console.log("FirstTag elemanın class güncellendi :", firstTag.className);
+
+// //! CLASSLİST
+
+// // console.log("firstTag elemanının ClassListi'i: ", firstTag.classList);
+
+// firstTag.classList.add("deneme"); // class eklemek icin
+// firstTag.classList.remove("test"); // class silmek için
+// firstTag.classList.replace("test-xl", "test-3xl"); // class güncelleme
+
+// // for (let cssClass of firstTag.classList) {
+// //    console.log("FirstTag Class'ı: ", cssClass);
+// // }
+
+// firstTag.classList.forEach((item) => {
+//    console.log("firstTag Class'ı:", item);
+// });
+
+// //************* */
+
+// let image = document.querySelector("#loading");
+// console.log(image);
+
+// console.log("Loading id'in İLK yüksekliği: ", image.getAttribute("height"));
+
+// function hideElm() {
+//    // image.setAttribute("width", 1);
+//    // image.setAttribute("height", 1);
+
+//    //! NORMAL SİLMEKK
+//    // image.style.display = "none";
+
+//    //! TAİLWİND İLE SİLMEK
+//    image.className = image.className + "hidden";
+
+//    //! GET ATTİRBUTE
+//    // console.log("Loading id'in SON yüksekliği: ", image.getAttribute("height"));
+
+//    //! REMOVE ATTİRBUTE
+//    firstTag.removeAttribute("target");
+// }
+
+// function showElm() {
+//    console.log(image.classList.contains("block"));
+
+//    image.classList.replace("hidden", "block");
+
+//    //! Class'ta var mı yok mu
+//    console.log(image.classList.contains("block"));
+// }
+
+// setTimeout(hideElm, 3000);
+
+// setTimeout(showElm, 6000);
+
+//!--------------------
+console.log("---------------- createElement ");
+
+let menuElm = document.querySelector("#menu");
+console.log(menuElm);
+console.log(menuElm.innerHTML); // etiketli getiri
+console.log(menuElm.textContent); // doğrudan metinsel olarak getirir
+
+let selamElm = document.querySelector(".selamTxt");
+function updateSelamText() {
+   selamElm.textContent = "Selam, Güncellendi!";
+
+   let newLiElement = document.createElement("li");
+   // newLiElement.textContent = "Yeni Eleman!";
+
+   let liLinkElm = document.createElement("a");
+   liLinkElm.setAttribute("href", "https://www.twitter.com");
+   liLinkElm.textContent = "Twitter";
+   newLiElement.appendChild(liLinkElm);
+
+   menuElm.appendChild(newLiElement);
+
+   //! AFTER METODU
+
+   let homeElm = document.querySelector(".homeElement");
+
+   let newPElm = document.createElement("p");
+   newPElm.textContent = "Yeni Bir P Elamanı";
+
+   homeElm.after(newPElm); // herhangi bir elemandan sonra eleman ekleme
+
+   //! REMOVE CHİLD
+
+   let silinecekEleman = document.querySelector("#merhabaTxt");
+   menuElm.removeChild(silinecekEleman); // elemanı silme
+
+   //! REPLACE CHİLD
+
+   let cikisYapElm = document.createElement("li");
+   cikisYapElm.textContent = "Çıkış Yap";
+
+   let girisYapElm = document.querySelector(".auth");
+   menuElm.replaceChild(cikisYapElm, girisYapElm);
 }
 
-setTimeout(divSekillendir, 5000);
+setTimeout(updateSelamText, 3000);
