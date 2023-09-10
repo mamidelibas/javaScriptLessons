00c0d7d160a0c8573ae9cf76715a9883d5debbdc
mamidelibas <mamidlbs@gmail.com>
2023-09-10 13:52:12 +0300

    jquery

--- JQUERY/jQuery.js
+++ JQUERY/jQuery.js
@@ -1,4 +1,17 @@
-let selamEl = document.getElementById("selam");
-let merhabaE = document.querySelector(".merhaba");
-console.log(selamEl);
-console.log(merhabaE);
+// let merhabaE = document.querySelector(".merhaba");
+// console.log(merhabaE);
+
+// let selamEl = document.getElementById("selam");
+// selamEl.addEventListener("click", function () {
+//   alert("Selam");
+// });
+// console.log(selamEl);
+
+//! JQUERY KULLANIMI
+
+// let selamEl = $("#selam");
+// console.log(selamEl);
+$("#selam").on("click", function (e) {
+  alert("Selam");
+});
+let merhabaEl = $(".merhaba");
